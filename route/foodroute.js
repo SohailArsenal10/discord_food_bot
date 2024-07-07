40123b059fd048aa7451817da55cbba2a63fb7f1
Syed Sohail Ahmed <sihailahmed1999@gmail.com>
2024-07-08 00:06:53 +0530

    Added queries

--- route/foodroute.js
+++ route/foodroute.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { foodRepository } from '../db/foodschema.js'
+import { foodoptionRepository, foodRepository } from '../db/foodschema.js'
 
 
 const router = Router();
@@ -38,13 +38,17 @@ name: 'pancake',
 ];
 
 let food1 = {
-  name: 'waffle',
-    description: 'waffle description',
+  name: 'pancake',
+    description: 'pancake description',
     thumbnail_url: '',
-    original_video_url: 'https://waffle.com',
-    instructions: ['Create waffle batter','Put it on stove and flip it','Serve with sauce'],
-    display: 'ChangePhoto'
+    original_video_url: 'https://egg.com',
+    instructions: ['Put batter','Put it on stove and flip it','Serve with syrup'],
+    display: 'ChangePhoto1'
   };
+
+  let foodoption = {
+     option: ['banana waffle','blueberry waffle','waffles']
+  }
 
 router.get('/:id', async (req, res) => {
   /*const person = await foodRepository.fetch(req.params.id)  //Food:01HSQQE5M9X3R5KQWSKD9A9VJC
@@ -60,6 +64,72 @@ router.get('/', async (req, res) => {
   res.json({
     code:200
   })
+})
+
+router.get('/search/foodRepository/:id', async (req, res) => {
+  //const foodall = await foodRepository.search().return.all()
+  const params = req.params;
+  const name = await foodRepository.search().where('name').matches(params.id).return.all()
+  /*const query = "@name:{pancake}"
+  const albums = await foodRepository.searchRaw(query).return.all();*/
+  res.send(name)
+})
+
+router.get('/search/foodoptionRepository/:id', async (req, res) => {
+  //const foodall = await foodRepository.search().return.all()
+  const params = req.params;
+  //const name = await foodoptionRepository.search().where('option').matches('egg').return.all()
+  /*const query = "@name:{pancake}"
+  const albums = await foodRepository.searchRaw(query).return.all();*/
+  const name = await foodoptionRepository.search().where('option').contain('*' + params.id + '*').return.all()
+  res.send(name)
+})
+
+router.get('/search/fooddescRepository/:id', async (req, res) => {
+  //const foodall = await foodRepository.search().return.all()
+  const params = req.params;
+  const name = await foodRepository.search().where('description').matches(params.id).return.all()
+  /*const query = "@name:{pancake}"
+  const albums = await foodRepository.searchRaw(query).return.all();*/
+  res.send(name)
+})
+
+router.get('/search/foodvideoRepository/:id', async (req, res) => {
+  //const foodall = await foodRepository.search().return.all()
+  const params = req.params;
+  const name = await foodRepository.search().where('original_video_url').matches(params.id).return.all()
+  /*const query = "@name:{pancake}"
+  const albums = await foodRepository.searchRaw(query).return.all();*/
+  res.send(name)
+})
+
+router.get('/search/foodimageRepository/:id', async (req, res) => {
+  //const foodall = await foodRepository.search().return.all()
+  const params = req.params;
+  const name = await foodRepository.search().where('thumbnail_url').matches(params.id).return.all()
+  /*const query = "@name:{pancake}"
+  const albums = await foodRepository.searchRaw(query).return.all();*/
+  res.send(name)
+})
+
+router.get('/search/foodinstRepository/:id', async (req, res) => {
+  //const foodall = await foodRepository.search().return.all()
+  const params = req.params;
+  //const name = await foodRepository.search().where('instructions').matches('egg').return.all()
+  /*const query = "@name:{pancake}"
+  const albums = await foodRepository.searchRaw(query).return.all();*/
+  const name = await foodRepository.search().where('instructions').contain('*' + params.id + '*').return.all()
+  res.send(name)
+})
+
+router.get('/option/:id', async (req, res) => {
+  /*const person = await foodRepository.fetch(req.params.id)  //Food:01HSQQE5M9X3R5KQWSKD9A9VJC
+  res.send(person)
+  console.log(req.params.id);*/
+
+  const person = await foodoptionRepository.save(foodoption)
+  res.send(person)
+  console.log(person);
 })
 
 router.post('/postfood', async (req, res) => {
